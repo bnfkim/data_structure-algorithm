7a37125a5dbf9f6451e967c7c9daa974f4ab3aa6
Gyuri Kim <kgr3819@gmail.com>
2022-04-25 15:32:22 +0900

    팩토리얼일반적론_python

--- 간단 알고리즘/factorial.py
+++ 간단 알고리즘/factorial.py
@@ -1,6 +1,16 @@
+#일반적론
+def factorial_normal(n):
+    first = 1
+    # 1부터 n까지 반복
+    for i in range(1, n+1):
+        first = first * i
+    return first
+
+#재귀함수
 def factorial(n):
     if n <= 1:
         return 1
     return factorial(n-1) * n
 
+print(factorial_normal(5))
 print(factorial(5))
